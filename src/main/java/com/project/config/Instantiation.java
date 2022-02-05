7f48c3719f5aeb22125fbf5c4bd316410d23d77d
all-an <all-an@hotmail.com>
2022-02-04 21:31:13 -0300

    authorDTO projection

--- src/main/java/com/project/config/Instantiation.java
+++ src/main/java/com/project/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.project.domain.Post;
 import com.project.domain.User;
+import com.project.dto.AuthorDTO;
 import com.project.repositories.PostRepository;
 import com.project.repositories.UserRepository;
 
@@ -35,8 +36,8 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob ));
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
